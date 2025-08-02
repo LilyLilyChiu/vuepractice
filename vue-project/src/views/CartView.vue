5ec1d065207d9dfe86e2b7967f204b58d62c75a9
LilyLilyChiu <Liling01327@gmail.com>
2025-08-02 11:43:01 +0800

    no message

---
+++ vue-project/src/views/CartView.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        這是購物車頁面
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+    
+</style>
---
+++ vue-project/src/views/ShopView.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        這是商城頁面
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+    
+</style>
