02bce91eb6c84f08dc58f1a64d1162e755bf5d90
LilyLilyChiu <Liling01327@gmail.com>
2025-08-02 11:42:52 +0800

    no message

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -4,6 +4,29 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
 
+    // === 主佈局 === 巢狀路由
+    {
+      // path 使用者在瀏覽器要輸入的路徑
+      path: "/main",
+      // component 要呈現的哪個頁面（元件）
+      component: () => import("@/layouts/MainLayout.vue"),
+      children: [
+        {
+          path: "shop",
+          component: () => import("@/views/ShopView.vue"),
+        },
+        {
+          path: "profile",
+          component: () => import("@/views/ProfileView.vue"),
+        },
+        {
+          path: "cart",
+          component: () => import("@/views/CartView.vue"),
+        },
+      ]
+    },
+
+
   ],
 })
 
